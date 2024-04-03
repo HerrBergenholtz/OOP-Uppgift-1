e8b55fe548d50724f6a1c3768c7f55f609b6ab46
HerrBergenholtz <zbdv8800@gmail.com>
2024-04-03 10:42:54 +0200

    Add project files.

---
+++ Klasser-Uppgift-1/Klass1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasser_Uppgift_1
+{
+    internal class Klass1
+    {
+        private string märke;
+        private string modell;
+        private string vikt;
+        private string färg;
+        private int hästkrafter;
+
+        public string setMärke
+        {
+            get { return märke; }
+            set { märke = value; }
+        }
+
+        public string setModell
+        {
+            get { return modell; }
+            set { modell = value; }
+        }
+
+        public string setVikt
+        {
+            get { return vikt; }
+            set { vikt = value; }
+        }
+
+        public string setFärg
+        {
+            get { return färg; }
+            set { färg = value; }
+        }
+
+        public int setHästkraft
+        {
+            get { return hästkrafter; }
+            set { hästkrafter = value; }
+        }
+    }   
+}
---
+++ Klasser-Uppgift-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Klasser_Uppgift_1;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Uppgift1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Klass1 bil = new();
+
+            bil.setMärke = "Volvo";
+            bil.setModell = "V70";
+            bil.setVikt = "1 ton";
+            bil.setFärg = "röd";
+            bil.setHästkraft = 500;
+
+            Console.WriteLine(bil.setMärke);
+            Console.WriteLine(bil.setModell);
+            Console.WriteLine(bil.setVikt);
+            Console.WriteLine(bil.setFärg);
+            Console.WriteLine(bil.setHästkraft);
+
+        }
+    }
+}
